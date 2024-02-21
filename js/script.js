a10d5539b77c9d926082af76b9555f8aa07fce01
HaasMatheus <e.matheushaas@gmail.com>
2024-02-21 19:08:24 -0300

    menu ativo js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//ativar links do Menu
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
